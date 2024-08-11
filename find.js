1edf5b194ba908db040a2f3d56068a499c2a65d1
BaekJunHyeok <wnsgur1832@naver.com>
2024-08-12 01:28:45 +0900

    240811

---
+++ find.js
@@ -0,0 +1,42 @@
+function findEmail() {
+  // '계정 찾기' 버튼 클릭 시 계정 찾기 폼 활성화
+  document.querySelector(".find-email-form").classList.add("active");
+  document.querySelector(".find-password-form1").classList.remove("active");
+
+  // '계정 찾기' 버튼 활성화, '비밀번호 찾기' 버튼 비활성화
+  document.querySelector(".email-btn").classList.add("active");
+  document.querySelector(".pw-btn").classList.remove("active");
+}
+
+function findPw() {
+  // '비밀번호 찾기' 버튼 클릭 시 비밀번호 찾기 폼 활성화
+  document.querySelector(".find-email-form").classList.remove("active");
+  document.querySelector(".find-password-form1").classList.add("active");
+
+  // '비밀번호 찾기' 버튼 활성화, '계정 찾기' 버튼 비활성화
+  document.querySelector(".pw-btn").classList.add("active");
+  document.querySelector(".email-btn").classList.remove("active");
+}
+
+// 기본적으로 계정 찾기 폼을 활성화
+document.addEventListener("DOMContentLoaded", function () {
+  findEmail();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const emailRadio = document.querySelector('input[name="pw1"][value="email"]');
+  const phoneRadio = document.querySelector('input[name="pw1"][value="phone"]');
+  const inputField = document.querySelector(
+    '.find-password-form1 input[type="text"]'
+  );
+
+  // 이메일 라디오 버튼 클릭 시
+  emailRadio.addEventListener("click", () => {
+    inputField.placeholder = "이메일 또는 회원 ID";
+  });
+
+  // 전화번호 라디오 버튼 클릭 시
+  phoneRadio.addEventListener("click", () => {
+    inputField.placeholder = "KR +82 | 전화번호를 입력하세요.";
+  });
+});
