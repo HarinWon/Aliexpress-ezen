ea47d7974eb676aa0ca639862282ecb9ec975094
BaekJunHyeok <wnsgur1832@naver.com>
2024-08-25 22:25:31 +0900

    240825

--- login.js
+++ login.js
@@ -10,6 +10,7 @@ function showQRLogin() {
   document.getElementById("qr-login").classList.add("active");
   document.getElementById("email_btn").classList.remove("active");
   document.getElementById("qr_btn").classList.add("active");
+  generateQRNumber();
 }
 showEmailLogin();
 
@@ -23,3 +24,66 @@ function checkScreenWidth() {
 
 window.addEventListener("load", checkScreenWidth);
 window.addEventListener("resize", checkScreenWidth);
+
+// qr 타이머
+let timeLeft = 180;
+let timerInterval;
+
+function updateTimer() {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  document.getElementById("qr-timer").innerText = `${minutes}:${seconds}`;
+
+  if (timeLeft <= 0) {
+    clearInterval(timerInterval);
+    document.getElementById("qr-timer").innerText = "00:00";
+    alert("시간이 만료되었습니다.");
+  } else {
+    timeLeft--;
+  }
+}
+
+function startQRTimer() {
+  clearInterval(timerInterval);
+  timeLeft = 180;
+  timerInterval = setInterval(updateTimer, 1000);
+}
+
+// qr숫자 랜덤
+function generateQRNumber() {
+  let qrNumber = Math.floor(Math.random() * 99) + 1;
+
+  qrNumber = qrNumber < 10 ? "0" + qrNumber : qrNumber;
+
+  document.getElementById("qr-number").innerText = qrNumber;
+}
+
+document.querySelector("#qr_btn").addEventListener("click", showQRLogin);
+
+// 비밀번호 유도
+document.addEventListener("DOMContentLoaded", function () {
+  const registeredEmail = "alieexpress@naver.com";
+  const registeredPhone = "01012345678";
+
+  const form = document.querySelector(".login-form");
+  const emailInput = document.getElementById("email-phone");
+  const passwordInput = document.getElementById("email-pw");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInput.value.trim();
+
+    if (enteredEmail === registeredEmail || enteredEmail === registeredPhone) {
+      passwordInput.style.display = "block";
+      passwordInput.setAttribute("required", "true");
+      emailInput.disabled = true;
+    } else {
+      window.location.href = "./loginAgree.html";
+    }
+  });
+});
