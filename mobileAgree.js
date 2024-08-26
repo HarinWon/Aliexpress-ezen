31903e808c4effb517ea706a216422d6af855f12
BaekJunHyeok <wnsgur1832@naver.com>
2024-08-27 00:04:16 +0900

    240827

--- mobileAgree.js
+++ mobileAgree.js
@@ -23,3 +23,9 @@ function checkScreenWidth() {
 
 window.addEventListener("load", checkScreenWidth);
 window.addEventListener("resize", checkScreenWidth);
+
+// back-button 누르면뒤로
+const backButton = document.querySelector(".back-button");
+backButton.addEventListener("click", () => {
+  window.history.back();
+});
--- mobileLogin.js
+++ mobileLogin.js
@@ -35,3 +35,39 @@ apple.addEventListener("click", () => {
     "https://appleid.apple.com/auth/authorize?client_id=com.alibaba.iAliexpressmsite&response_type=code%20id_token&scope=email%20name&response_mode=form_post&state=iV%2FFZnOchXPmwM5RItjn6HmubjMUMB%2FVwK3y5c1OwFs1ER8lbn1uYILSqJElhhSudo4iKihQMc2LvwkeMh1fwtq%2Fge2mJLeDVFxbOqexCQfbBW4RzjUmfJRC9z16J8mh%2BYtvmVMIRAHTq5VAkBXbol1OSC58oZnIuGepibXNolyqG%2BdFvfyYuwlHvHePOctonzPLwT4CeQo2%2BgvDg59FEg%3D%3D&redirect_uri=https%3A%2F%2Fthirdparty.aliexpress.com%2Fapplecallback.htm";
   window.location.href = targetUrl;
 });
+
+// 유효성검사
+function validateForm() {
+  const userId = document.getElementById("userId").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const phoneRegex = /^010\d{8}$/;
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d|.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$/;
+  const consecutiveCharsRegex = /(.)\1\1/;
+
+  let isIdValid = emailRegex.test(userId) || phoneRegex.test(userId);
+  let isPasswordValid =
+    passwordRegex.test(password) && !consecutiveCharsRegex.test(password);
+
+  if (!isIdValid) {
+    document.getElementById("userId").placeholder =
+      "이메일 주소나 휴대폰 번호를 잘못 적었습니다";
+    document.getElementById("userId").value = "";
+  }
+
+  if (!isPasswordValid) {
+    document.getElementById("password").placeholder =
+      "8~16자, 영문, 숫자, 특수문자2자 이상 사용하세요";
+    document.getElementById("password").value = "";
+  }
+
+  if (isIdValid && isPasswordValid) {
+    if (userId === "alieexpress@naver.com" || userId === "01012345678") {
+      window.location.href = "congratulate.html";
+    } else {
+      window.location.href = "mobileAgree.html";
+    }
+  }
+}
