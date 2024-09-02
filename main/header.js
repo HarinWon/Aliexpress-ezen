9fac0f3b9a178848563afbb731a57fa7a8df3088
yerixxz <yerixxz@gmail.com>
2024-09-02 11:51:34 +0900

    240902

--- main/header.js
+++ main/header.js
@@ -291,3 +291,29 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// 서브 네비 스크롤 시 없애는 코드
+document.addEventListener("DOMContentLoaded", function () {
+  var lastScrollTop = 0; // 마지막 스크롤 위치를 저장할 변수
+  var headerNav = document.querySelector(".header-nav"); // header-nav 요소 선택
+
+  window.addEventListener("scroll", function () {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop; // 현재 스크롤 위치
+
+    if (scrollTop > lastScrollTop) {
+      // 아래로 스크롤할 경우
+      headerNav.style.opacity = Math.max(0, headerNav.style.opacity - 0.1);
+      if (headerNav.style.opacity <= 0) {
+        headerNav.style.display = "none";
+      }
+    } else {
+      // 위로 스크롤할 경우
+      headerNav.style.display = "block";
+      headerNav.style.opacity = Math.min(
+        1,
+        parseFloat(headerNav.style.opacity) + 0.1
+      );
+    }
+    lastScrollTop = scrollTop; // 스크롤 위치 업데이트
+  });
+});
